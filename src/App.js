bea841b3a52a77e18dc96c537065e9ee2a6cf415
JovHariono <128022304+JovHariono@users.noreply.github.com>
2023-09-15 22:08:40 +0700

    buat detail blog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import BlogDetail from './BlogDetail';
+import Create from './Create';
 import Home from './Home';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <div className='content'>
-        <Home />
+      <div className='content'>        
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetail />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 export default App;
--- src/useFetch.js
+++ src/useFetch.js
@@ -2,19 +2,28 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useFetch = (url) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
+    const abort = new AbortController()
+
     axios
-      .get(url, {
+      .get(url, { signal: abort.signal }, {
         withCredentials: true,
       })
       .then((res) => {
         setData(res.data);
-        setIsPending(false);
+        setIsPending(false);        
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        if(err.message === "AbortError"){
+          console.log("fetch aborted")
+        }
+        else{
+          console.log(err)
+        }
+      });
   }, [url]);
 
   return{ data, isPending }
