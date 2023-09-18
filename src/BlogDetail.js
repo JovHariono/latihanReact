477b5b85132c94044e7f1483c88de1409073154f
JovHariono <128022304+JovHariono@users.noreply.github.com>
2023-09-18 20:50:24 +0700

    tambah delete dan tambah page not found

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,20 +1,33 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetail = () => {
   const { id } = useParams();
   const { data, isPending } = useFetch(`http://localhost:8000/blogs/` + id);
+  const history = useHistory();
+
+  const handleDelete = () => {
+    axios
+      .delete(`http://localhost:8000/blogs/` + data.id)
+      .then((res) => {        
+        history.push("/");
+      })
+      .catch((err) => console.log(err.data));
+  };
 
   return (
     <div className="blog-details">
-      { isPending && <div>Loading...</div> }
-      { data && (
+      {isPending && <div>Loading...</div>}
+      {data && (
         <article>
-            <h2> { data.title } </h2>
-            <p> Written by { data.author } </p>
-            <div> { data.body } </div>
+          <h2> {data.title} </h2>
+          <p> Written by {data.author} </p>
+          <div> {data.body} </div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
-      ) }
+      )}
     </div>
   );
 };
